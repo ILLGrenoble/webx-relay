ccc185055888ec333c053619cfd5b0991eba7c99
Stuart Caunt <caunt@ill.fr>
2022-02-11 10:12:12 +0100

    Log token generation

--- src/main/java/eu/ill/webx/services/AuthService.java
+++ src/main/java/eu/ill/webx/services/AuthService.java
@@ -1,6 +1,8 @@
 package eu.ill.webx.services;
 
 import eu.ill.webx.model.Credentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -8,6 +10,8 @@ import java.util.Map;
 import java.util.UUID;
 
 public class AuthService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
 
     private static final AuthService instance = new AuthService();
 
@@ -26,10 +30,13 @@ public class AuthService {
         this.thread.start();
     }
 
-    public synchronized String addAuthorisation(String credentials) {
+    public synchronized String addAuthorisation(String raw) {
         String token = UUID.randomUUID().toString().replace("-", "");;
-        this.authorisations.put(token, new Credentials(credentials));
+        Credentials credentials = new Credentials(raw);
 
+        this.authorisations.put(token, credentials);
+
+        logger.info("Generated token {} for {}", token, credentials.getUsername());
         return token;
     }
 
